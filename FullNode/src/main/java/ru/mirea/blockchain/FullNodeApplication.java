04e8e2a6c227091e1f563abc957097e1132a23e4
Денис <dskomissarov@istokmw.ru>
2025-01-17 15:08:21 +0300

    Merge remote-tracking branch 'origin/dev' into dev

---
+++ FullNode/src/main/java/ru/mirea/blockchain/FullNodeApplication.java
@@ -0,0 +1,11 @@
+package ru.mirea.blockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullNodeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FullNodeApplication.class);
+    }
+}
